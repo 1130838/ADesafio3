5923d9b6b5a10e4b4dfe72362c642109810d84c7
bruno.devesa <1130838@isep.ipp.pt>
2016-03-18 10:34:43 +0000

    added XML string-array in the ArrayList controller.   added XL string resource in textView label

--- app/src/main/java/com/brunodevesa/adesafio3/MainActivity.java
+++ app/src/main/java/com/brunodevesa/adesafio3/MainActivity.java
@@ -1,20 +1,19 @@
 package com.brunodevesa.adesafio3;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,17 +24,20 @@ public class MainActivity extends AppCompatActivity {
     ListView mListView;
     TextView textView ;
     List<String> things;
-
+    String[] resourcesArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        things = new ArrayList<>();
-        things.add("First thing");
+        resourcesArray = getResources().getStringArray(R.array.students_array);
+
+        things = Arrays.asList(resourcesArray);
+
+       /* things.add("First thing");
         things.add("Second thing");
-        things.add("Third thing");
+        things.add("Third thing");*/
 
         mListView = (ListView)findViewById(R.id.listView);
         textView = (TextView)findViewById(R.id.textView);
